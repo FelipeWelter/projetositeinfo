0cca47b640edfc9178fe2518d285e034436aff47
FelipeWelter <feliwelter45@gmail.com>
2025-06-27 19:33:50 -0300

    upload backup 1 dia 27062025

---
+++ app/models.py
@@ -0,0 +1,22 @@
+from . import db
+from datetime import datetime
+
+class Servico(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    titulo = db.Column(db.String(100), nullable=False)
+    descricao = db.Column(db.Text, nullable=False)
+    data_criacao = db.Column(db.DateTime, default=datetime.utcnow)
+
+class Produto(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(100), nullable=False)
+    preco = db.Column(db.Float, nullable=False)
+    descricao = db.Column(db.Text)
+    data_criacao = db.Column(db.DateTime, default=datetime.utcnow)
+
+class Post(db.Model):  # Notícias ou artigos
+    id = db.Column(db.Integer, primary_key=True)
+    titulo = db.Column(db.String(150), nullable=False)
+    conteudo = db.Column(db.Text, nullable=False)
+    autor = db.Column(db.String(50), default="Admin")
+    data_postagem = db.Column(db.DateTime, default=datetime.utcnow)
--- config.py
+++ config.py
@@ -1,4 +1,7 @@
+import os
+
 class Config:
     SECRET_KEY = 'sua_chave_segura_aqui'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+    BASEDIR = os.path.abspath(os.path.dirname(__file__))
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASEDIR, 'site.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
